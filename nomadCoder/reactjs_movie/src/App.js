b0e0c438a16f37af74a662c6b474996b4e450919
jeonga lee <lja3248@gmail.com>
2022-05-10 16:40:06 +0900

    [feat] add to useEffect and cleanup Function

--- nomadCoder/reactjs_movie/src/App.js
+++ nomadCoder/reactjs_movie/src/App.js
@@ -1,11 +1,19 @@
-import Button from "./Button.js";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+function Hello() {
+  useEffect(() => {
+    console.log("나타났습니당");
+    return () => console.log("숨겼습니당");
+  }, []);
+  return <h1>Hello</h1>;
+}
 
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"continue"} />
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
     </div>
   );
 }
